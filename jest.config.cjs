08732e764e05e2cfda14309436749c34ed4b1961
keiner <keiner3462@gmail.com>
2025-09-02 00:03:38 -0500

    keiner prueba

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,16 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    setupFiles: ['./jest.setup.js']
-}
+  testEnvironment: "jsdom",
+  setupFiles: ["./jest.setup.js"],
+
+  // Ignorar CSS, imágenes y otros assets
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg|png|jpg|jpeg)$": "<rootDir>/__mocks__/fileMock.js"
+  },
+
+  // Evita que Jest intente transformar node_modules pesados
+  transformIgnorePatterns: ["/node_modules/"],
+
+  // Opcional: evita que Jest truene si no encuentra coverage
+  collectCoverage: false
+};
