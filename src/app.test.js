83ca3d83283dc5dae657885f57faadc77d21b011
keiner <keiner3462@gmail.com>
2025-09-01 23:45:22 -0500

    Fix: ajustes en jest y archivo de test

---
+++ src/app.test.js
@@ -0,0 +1,7 @@
+// src/App.test.js
+import { render } from '@testing-library/react';
+import { GifExpertApp } from './GifExpertApp';
+
+test('renders <GifExpertApp /> without crashing', () => {
+  render(<GifExpertApp />);
+});
